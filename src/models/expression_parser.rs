c54012c3e3aae525da1cf47e489f6672bdcbce5b
Eusebio <eusebiodm98@gmail.com>
2019-11-05 00:14:18 -0300

    Evaluation parser works

--- src/models/expression_parser.rs
+++ src/models/expression_parser.rs
@@ -1,6 +1,6 @@
 #[allow(unused_variables)]
 use std::collections::HashMap;
-use evalexpr::{build_operator_tree, Node, Context, HashMapContext};
+use evalexpr::{build_operator_tree, Node, Context, HashMapContext, Value};
 use crate::server::ApiResult;
 
 pub struct PromotionExpression {
@@ -10,12 +10,17 @@ pub struct PromotionExpression {
 impl PromotionExpression {
     pub fn parse(code: &str) -> ApiResult<Self> {
         debug_assert_eq!(code, code.to_lowercase()); // Code must be all lower case
-        let mut expr = code.trim_start_matches("if");
-        expr = expr.trim_end_matches("then apply_discount");
-        let e = expr.replace("\n", "");
-        let ast = build_operator_tree(&e)?;
+        let expr = Self::transform_expression(code);
+        let ast = build_operator_tree(&expr)?;
 
         Ok(PromotionExpression { ast })
+    }
+
+    fn transform_expression(raw: &str) -> String {
+        raw.trim_start_matches("if")
+            .trim_end_matches("then apply_discount")
+            .replace("and", "&&")
+            .replace("or", "||")
     }
 
     pub fn evaluate<T>(&self, attributes: HashMap<T, f64>) -> ApiResult<bool> where T: Into<String> {
@@ -23,7 +28,10 @@ impl PromotionExpression {
         for (key, val) in attributes {
             context.set_value(key.into(), val.into())?;
         }
-        let result = self.ast.eval_boolean_with_context(&context)?;
+        context.set_value("valid_coupon_code".into(), Value::Boolean(true))?;
+        context.set_value("valid_transaction".into(), Value::Boolean(true))?;
+
+        let result = self.ast.eval_boolean_with_context_mut(&mut context)?;
 
         Ok(result)
     }
@@ -43,32 +51,32 @@ mod tests {
 
     #[test]
     fn valid_discount_code() {
-        let exp = coupon_expression();
-        let attributes = hashmap!["total" => 500.0, "quantity" => 4.0];
-        let result = exp.evaluate(attributes).unwrap();
-        assert!(result)
-    }
-
-    #[test]
-    fn valid_discount_code_or() {
-        let exp = coupon_expression();
+        let exp = discount_expression();
         let attributes = hashmap!["total" => 10000.0, "quantity" => 4.0];
         let result = exp.evaluate(attributes).unwrap();
         assert!(result)
     }
 
     #[test]
-    fn invalid_discount_code() {
-        let exp = coupon_expression();
-        let attributes = hashmap!["total" => 150.0, "products_size" => 10.0];
+    fn valid_discount_code_or() {
+        let exp = discount_expression();
+        let attributes = hashmap!["total" => 250.0, "quantity" => 6.0];
         let result = exp.evaluate(attributes).unwrap();
         assert!(result)
     }
 
     #[test]
+    fn invalid_discount_code() {
+        let exp = discount_expression();
+        let attributes = hashmap!["total" => 150.0, "quantity" => 2.0];
+        let result = exp.evaluate(attributes).unwrap();
+        assert!(!result)
+    }
+
+    #[test]
     fn valid_coupon_code() {
         let exp = coupon_expression();
-        let attributes = hashmap!["total" => 50.0, "products_size" => 1.0];
+        let attributes = hashmap!["total" => 101.0, "products_size" => 3.0];
         let result = exp.evaluate(attributes).unwrap();
         assert!(result)
     }
@@ -78,7 +86,7 @@ mod tests {
         let exp = coupon_expression();
         let attributes = hashmap!["total" => 150.0, "products_size" => 1.0];
         let result = exp.evaluate(attributes).unwrap();
-        assert!(result)
+        assert!(!result)
     }
 
     fn coupon_expression() -> PromotionExpression {
@@ -90,9 +98,11 @@ mod tests {
     }
 
     fn discount_expression() -> PromotionExpression {
-        let code = "if valid_transaction (1)
-                          or (total <= 1000 AND quantity >= 5) (2)
-                          or total > 1000
+        let code = "if valid_transaction
+                          and (
+                            (total <= 1000 and quantity >= 5)
+                            or
+                            total > 1000)
                           then apply_discount";
         PromotionExpression::parse(code).unwrap()
     }
